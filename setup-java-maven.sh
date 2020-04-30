9ec3b030480711e6cbacddf14a89f4191a705a99
naridnevahgar <48238381+naridnevahgar@users.noreply.github.com>
2020-04-30 13:23:54 +0900

    force install

--- setup-java-maven.sh
+++ setup-java-maven.sh
@@ -2,9 +2,9 @@
 
 sudo apt update
 # Install the open jdk first
-sudo apt install default-jdk
+sudo apt install default-jdk -y
 
 # Install maven
-sudo apt install maven
+sudo apt install maven -y
 
 mvn -version
